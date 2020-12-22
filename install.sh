d3a563ba896544ff022097ce8236e526c14acf81
WhoamiMiss <yangwl2112@mails.jlu.edu.cn>
2020-12-22 15:02:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
-sudo apt install gdb gcc g++ python3 python3-pip build-essential 
+sudo apt install gdb gcc g++ python3 python3-pip build-essential  python3-dev git libssl-dev libffi-dev 
 
  
+#pip configure
+mkdir ~/.pip 
+touch ~/.pip/pip.conf 
+cat <<EOT >> ~/.pip/pip.conf
+[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+[install]
+trusted-host=mirrors.aliyun.com
+EOT
 
-cd ~ && mkdir .pip 
-cd ~/.pip && touch pip.conf 
- 
+cd ~/
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+
+#setup tools
+pip3 install -U setuptools
+pip3 install --upgrade pip
+pip3 install --upgrade pwntools
