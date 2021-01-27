e2b8a1274e27c66becd106853a7a4b6b711e7ed2
WhoamiMiss <yangwl2112@mails.jlu.edu.cn>
2021-01-27 10:44:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@ alias por='export http_proxy="http://127.0.0.1:1081/" https_proxy="http://127.0.
 alias ppor='export http_proxy="" https_proxy=""'
 export http_proxy="http://127.0.0.1:1081/" https_proxy="http://127.0.0.1:1081/"
 
+mkdir -p reversetools
+cd reversetools
+wget https://raw.githubusercontent.com/slimm609/checksec.sh/master/checksec
+chmod a+x checksec
+
+mv checksec ~
 sudo apt install -y gdb gcc g++ python3 python3-pip build-essential  python3-dev git libssl-dev libffi-dev 
 ln -s /usr/bin/python3 /usr/bin/python
 ln -s /usr/bin/pip3 /usr/bin/pip
